8964e781474d0b17d7757fd1c5bf34c248f427e1
TANISHQ ARORA <72154097+DeveloperTanishq@users.noreply.github.com>
2022-01-18 09:56:51 +0530

    Added APIs
    routes,services,config for files

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const path = require('path')
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+// DB 
+const connectDB = require('./config/db');
+connectDB();
+
+//Template Engine
+
+app.set('views',path.join(__dirname, '/views'));
+app.set('view engine', 'ejs')
+
+//Routes
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/files/download', require('./routes/download'));
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`);
+})
