7b5090b78f019c3da6a7f82f0b48b3d2d9969821
TANISHQ ARORA <72154097+DeveloperTanishq@users.noreply.github.com>
2022-01-18 11:37:57 +0530

    Added CORS

--- server.js
+++ server.js
@@ -6,11 +6,22 @@ const PORT = process.env.PORT || 3000;
 
 const path = require('path')
 
+const cors = require('cors');
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 // DB 
 const connectDB = require('./config/db');
 connectDB();
+
+//CORS
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+
+app.use(cors(corsOptions))
+
 
 //Template Engine
 
